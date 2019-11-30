a129b022a93ee81ddabd4d175c5b8ea2bd1852df
Juan <a00815058@itesm.mx>
2019-11-30 10:05:35 -0600

    Add description te event model

--- models/eventModel.js
+++ models/eventModel.js
@@ -9,6 +9,10 @@ var eventSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  description: {
+      type: String,
+      required: true
+  },
   date:{
       type: Date,
       required: true
