84c6f44a55fa810990ca39b89b04991c7ea684f3
Juan <a00815058@itesm.mx>
2019-11-30 10:06:01 -0600

    Create crud register

--- controlers/registerControler.js
+++ controlers/registerControler.js
@@ -25,7 +25,39 @@ const getRegisterByUserID = function(req, res){
     })
 }
 
+//Update register
+const updateRegister = function(req, res){
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["services"]
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidUpdate){
+        return res.status(400).send({
+            error: 'Invalid update, only allowed updates: ' + allowedUpdates
+        })
+    }
+    Register.findByIdAndUpdate(_id, req.body).then(function(register){
+        if(!register){
+            return res.status(404).send()
+        }
+        return res.send(register)
+    }).catch(function(error){
+        res.status(500).send(error)
+    })
+}
+
+//Delete register
+const deleteRegister = function(req, res){
+    const id = req.params.id
+    Register.findOneAndRemove({ _id: id }).then(function(event,res){
+        res.send({event: "deleted"});
+    }).catch(function(error){
+        res.status(500).send(error)
+    })
+}
+
 module.exports = { 
     createRegister: createRegister,
-    getRegisterByUserID: getRegisterByUserID
+    getRegisterByUserID: getRegisterByUserID,
+    deleteRegister: deleteRegister
 }
